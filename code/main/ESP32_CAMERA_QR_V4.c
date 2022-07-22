7c1bb2d356b4480477b22e5d1b5722162eb58971
Ataa Daghstani <ataa@zwsdev.onmicrosoft.com>
2022-07-22 19:35:56 +0300

    add TODO item in main code

--- code/main/ESP32_CAMERA_QR_V4.c
+++ code/main/ESP32_CAMERA_QR_V4.c
@@ -3,4 +3,11 @@
 
 void app_main(void)
 {
+    // TODO image capture function
+    // TODO add qr_recoginze function after the image is captured
+
+    //#if CONFIG_QR_RECOGNIZE
+    // camera_config_t *camera_config = ctx;
+    // xTaskCreate(qr_recoginze, "qr_recoginze", 111500, camera_config, 5, NULL);
+    //#endif
 }
